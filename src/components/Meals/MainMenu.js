aa3994b5903c5e2a4d7ce280a995eb901bf46aae
melnard <dejesusmelnard@gmail.com>
2023-06-12 21:06:21 +0800

    use fetch api to fetch data from firebase realtime database

--- src/components/Meals/MainMenu.js
+++ src/components/Meals/MainMenu.js
@@ -1,15 +1,60 @@
-import React, { useState } from "react";
-import Data from "../../Data/Data";
+import React, { useState, useEffect, useCallback } from "react";
+// import Data from "../../Data/Data";
 import MealItem from "./MealItem/MealItem";
 import FilterMeals from "./FilterMeals";
 import classes from "./MainMenu.module.scss";
 
 const MainMenu = () => {
-  const [filterFoodList, setFilterFoodList] = useState(Data);
+  const [menu, setMenu] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  // state for filtering items
+  const [filterFoodList, setFilterFoodList] = useState([]);
+
+  const fetchMenuHandler = useCallback(async () => {
+    setHttpError(null);
+
+    try {
+      const response = await fetch(
+        "https://react-food-app-5afee-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          category: data[key].category,
+          image: data[key].image,
+          price: data[key].price,
+        });
+      }
+
+      // console.log(loadedMeals);
+
+      setMenu(loadedMeals);
+    } catch (error) {
+      // console.log(error.message);
+      setHttpError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMenuHandler();
+  }, [fetchMenuHandler]);
 
   const itemListFilterHandler = (category) => {
     setFilterFoodList(
-      Data.filter((item) => {
+      menu.filter((item) => {
         if (category === "all") {
           return item.category !== category;
         } else {
@@ -17,24 +62,52 @@ const MainMenu = () => {
         }
       })
     );
+    console.log("filter items");
   };
 
-  const foodsList = filterFoodList.map((meal) => {
-    const { id, name, category, image, price } = meal;
-
+  const foodsList = menu.map((meal) => {
     return (
       <MealItem
-        key={id}
-        id={id}
-        name={name}
-        category={category}
-        image={image}
-        price={price}
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        category={meal.category}
+        image={meal.image}
+        price={meal.price}
       />
     );
   });
 
-  let content = <ul>{foodsList}</ul>;
+  const filteredFoodList = filterFoodList.map((meal) => {
+    return (
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        category={meal.category}
+        image={meal.image}
+        price={meal.price}
+      />
+    );
+  });
+
+  let content;
+
+  if (menu.length > 0 && filterFoodList.length === 0) {
+    content = <ul>{foodsList}</ul>;
+  } else if (menu.length > 0 && filterFoodList.lenght !== 0) {
+    content = <ul>{filteredFoodList}</ul>;
+  }
+
+  if (isLoading) {
+    content = <p className={classes.error}>Loading..</p>;
+  }
+
+  if (httpError) {
+    content = <p className={classes.error}>{httpError}</p>;
+  }
+
+  console.log("rerendering this component");
 
   return (
     <section className={classes.meals}>
